96c21012a4551ff1bb674b4f3cb6b9656dc3bb24
Pavan Reddy Kollu <pavanreddy0861@gmail.com>
2025-04-25 13:44:53 +0530

    Changes related to rendering bar chart

--- forecasting-ui/src/components/Home.jsx
+++ forecasting-ui/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Box } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useAuth } from '../context/AuthContext';
@@ -6,13 +6,33 @@ import UploadArea from '../components/UploadArea';
 import LoadingScreen from '../components/LoadingScreen';
 import { uploadFile } from '../api/file';
 import { DataGrid } from '@mui/x-data-grid';
+import { CartesianGrid, XAxis, YAxis, Bar, BarChart, Tooltip} from 'recharts'
+// import { BarChart, Bar } from '@mui/x-charts/BarChart';
+// import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "@material-ui/x-charts";
+
+const initialRows = [
+  { id: 1, name: 'Category A', value: 15 },
+  { id: 2, name: 'Category B', value: 25 },
+  { id: 3, name: 'Category C', value: 35 }
+];
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Category', width: 150 },
+  { field: 'value', headerName: 'Value', width: 150 }
+];
 
 export default function Home() {
   const { logout } = useAuth();
   const [loading, setLoading] = useState(false);
   const [rows, setRows] = useState([]);
   const [cols, setCols] = useState([]);
-
+  const [chartData,setChartdata] = useState([]);
+  const [chartLabels, setChartLabels] = useState([]);
+  useEffect(() => {
+    setChartdata(rows.map((row) => row.value)) 
+    setChartLabels(rows.map((row) => row.name))
+  } ,[rows, cols])
   const handleFile = async file => {
     setLoading(true);
     try {
@@ -40,6 +60,19 @@ export default function Home() {
           <DataGrid rows={rows} columns={cols} pageSize={10} rowsPerPageOptions={[10]} />
         )}
       </Box>
+      <div style={{ height: 400, width: 400 }}>
+      {!loading && rows.length > 0 && (
+        <BarChart width={600} height={400} data={rows}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="Date" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="Temp Min" fill="#8884d8" />
+      
+        <Bar dataKey="Temp Max" fill="#82ca9d" />
+      </BarChart>
+      )}
+      </div>
     </Box>
   );
 }
